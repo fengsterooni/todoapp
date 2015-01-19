5e17072fef8e50b26d506bfcf27403df5520a08c
Feng Guo <fengsterooni@gmail.com>
2015-01-18 21:07:26 -0800

    first step

--- app/src/main/java/com/codepath/todoapp/activities/TodoActivity.java
+++ app/src/main/java/com/codepath/todoapp/activities/TodoActivity.java
@@ -3,27 +3,26 @@ package com.codepath.todoapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.codepath.todoapp.R;
-import com.codepath.todoapp.database.TodoItemDatabase;
 import com.codepath.todoapp.adapters.TodoAdapter;
+import com.codepath.todoapp.database.TodoItemDatabase;
 import com.codepath.todoapp.models.TodoItem;
 
 import java.util.ArrayList;
 
 
-public class TodoActivity extends ActionBarActivity {
+public class TodoActivity extends ActionBarActivity
+        implements TodoAdapter.ItemClickListener{
     private static final String TAG = TodoActivity.class.getSimpleName();
 
     private ArrayList<TodoItem> todoItems;
-    private ArrayAdapter<TodoItem> todoAdapter;
-    private ListView lvItems;
+    private TodoAdapter todoAdapter;
+    private RecyclerView recyclerView;
     private static final int ITEM_EDIT_REQUEST = 1;
     private static final int ITEM_ADD_REQUEST = 2;
     private TodoItemDatabase db;
@@ -33,18 +32,29 @@ public class TodoActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
 
+        recyclerView = (RecyclerView) findViewById(R.id.lvItem);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
         // Initialize the Database
         db = new TodoItemDatabase(this);
 
         // Read the TodoItem from the Database
         todoItems = db.getAllTodoItems();
-        todoAdapter = new TodoAdapter(this, todoItems);
+        todoAdapter = new TodoAdapter(todoItems, this);
 
         // Inflate the ListView
-        lvItems = (ListView) findViewById(R.id.lvItem);
-        lvItems.setAdapter(todoAdapter);
+        recyclerView.setAdapter(todoAdapter);
 
-        setupListViewListener();
+        // setupListViewListener();
+    }
+
+    @Override
+    public void itemClicked(TodoItem todoItem) {
+        Intent intent = new Intent(TodoActivity.this, EditItemActivity.class);
+        intent.putExtra("item", todoItem);
+        startActivityForResult(intent, ITEM_EDIT_REQUEST);
     }
 
     @Override
@@ -64,7 +74,7 @@ public class TodoActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_todo, menu);
         return true;
     }
-
+/*
     private void setupListViewListener() {
         lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -90,6 +100,8 @@ public class TodoActivity extends ActionBarActivity {
             }
         });
     }
+
+    */
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
--- app/src/main/java/com/codepath/todoapp/adapters/TodoAdapter.java
+++ app/src/main/java/com/codepath/todoapp/adapters/TodoAdapter.java
@@ -1,10 +1,11 @@
 package com.codepath.todoapp.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,64 +13,121 @@ import com.codepath.todoapp.R;
 import com.codepath.todoapp.models.TodoItem;
 import com.codepath.todoapp.utils.DateUtils;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class TodoAdapter extends ArrayAdapter<TodoItem> {
-    public TodoAdapter(Context context, ArrayList<TodoItem> items) {
-        super(context, R.layout.item_todo, items);
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
+
+    private List<TodoItem> todoItems;
+    private ItemClickListener itemClickListener;
+
+    public TodoAdapter(List<TodoItem> objects, @NonNull ItemClickListener itemClickListener) {
+        this.todoItems = objects;
+        this.itemClickListener = itemClickListener;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final TodoItem todoItem = getItem(position);
-
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_todo, parent, false);
-            viewHolder.icon = (ImageView) convertView.findViewById(R.id.ivItemPriority);
-            viewHolder.title = (TextView) convertView.findViewById(R.id.tvItemTitle);
-            viewHolder.notes = (TextView) convertView.findViewById(R.id.tvItemNotes);
-            viewHolder.dueMonth = (TextView) convertView.findViewById(R.id.tvItemDueMonth);
-            viewHolder.dueDay = (TextView) convertView.findViewById(R.id.tvItemDueDay);
-
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        String priority = todoItem.getPriority();
-        switch (priority) {
-            case "High":
-                viewHolder.icon.setImageResource(R.drawable.high32);
-                break;
-            case "Default":
-                viewHolder.icon.setImageResource(R.drawable.default32);
-                break;
-            case "Low":
-                viewHolder.icon.setImageResource(R.drawable.low32);
-                break;
-            default:
-                viewHolder.icon.setImageResource(R.drawable.unset32);
-        }
-
-        viewHolder.title.setText(todoItem.getTitle());
-        viewHolder.notes.setText(todoItem.getNotes());
-
-        Date date = todoItem.getDueDate();
-        viewHolder.dueMonth.setText(DateUtils.getMonthString(date));
-        viewHolder.dueDay.setText(DateUtils.getDayString(date));
-
-        return convertView;
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        Context context = viewGroup.getContext();
+        View parent = LayoutInflater.from(context).inflate(R.layout.item_todo, viewGroup, false);
+        return ViewHolder.newInstance(parent);
     }
 
-    private static class ViewHolder {
-        ImageView icon;
-        TextView title;
-        TextView notes;
-        TextView dueMonth;
-        TextView dueDay;
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        final TodoItem todoItem = todoItems.get(position);
+        viewHolder.setIcon(todoItem.getPriority());
+        viewHolder.setTitle(todoItem.getTitle());
+        viewHolder.setNotes(todoItem.getNotes());
+        Date date = todoItem.getDueDate();
+        viewHolder.setDueMonth(DateUtils.getMonthString(date));
+        viewHolder.setDueDay(DateUtils.getDayString(date));
+        viewHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemClickListener.itemClicked(todoItem);
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return todoItems.size();
+    }
+
+    public static final class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+        private final View parent;
+        private final ImageView icon;
+        private final TextView title;
+        private final TextView notes;
+        private final TextView dueMonth;
+        private final TextView dueDay;
+        private ItemClickListener itemClickListener;
+
+        public static ViewHolder newInstance(View parent) {
+            ImageView icon = (ImageView) parent.findViewById(R.id.ivItemPriority);
+            TextView title = (TextView) parent.findViewById(R.id.tvItemTitle);
+            TextView notes = (TextView) parent.findViewById(R.id.tvItemNotes);
+            TextView dueMonth = (TextView) parent.findViewById(R.id.tvItemDueMonth);
+            TextView dueDay = (TextView) parent.findViewById(R.id.tvItemDueDay);
+            parent.setOnClickListener(this);
+            parent.setOnLongClickListener(this);
+
+            return new ViewHolder(parent, icon, title, notes, dueMonth, dueDay);
+        }
+
+        private ViewHolder(View parent, ImageView icon,
+                TextView title, TextView notes, TextView dueMonth, TextView dueDay) {
+            super(parent);
+            this.parent = parent;
+            this.icon = icon;
+            this.title = title;
+            this.notes = notes;
+            this.dueMonth = dueMonth;
+            this.dueDay = dueDay;
+        }
+
+        public void setIcon(String text) {
+            int imageId;
+            switch (text) {
+                case "High":
+                    imageId = R.drawable.high32;
+                    break;
+                case "Default":
+                    imageId = R.drawable.default32;
+                    break;
+                case "Low":
+                    imageId = R.drawable.low32;
+                    break;
+                default:
+                    imageId = R.drawable.unset32;
+            }
+            icon.setImageResource(imageId);
+        }
+        public void setTitle(CharSequence text) {
+            title.setText(text);
+        }
+        public void setNotes(CharSequence text) {
+            notes.setText(text);
+        }
+        public void setDueMonth(CharSequence text) {
+            dueMonth.setText(text);
+        }
+        public void setDueDay(CharSequence text) {
+            dueDay.setText(text);
+        }
+
+        public void setOnClickListener(View.OnClickListener listener) {
+            parent.setOnClickListener(listener);
+        }
+    }
+
+    public interface ItemClickListener {
+        public void itemClicked(TodoItem todoItem, View view, int position, boolean isLongClick);
+
+        public void setItemClickListener(ItemClickListener clickListener) {
+            this.c
+        }
     }
 }
